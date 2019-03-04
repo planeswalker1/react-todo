770c0999d52b6e12c9a0ff2a09e1f1935c1bfbec
Daniel Munoz <daniel.munozdev@gmail.com>
2019-03-03 21:47:39 -0800

    add start files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="center blue-text">Todos:</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
