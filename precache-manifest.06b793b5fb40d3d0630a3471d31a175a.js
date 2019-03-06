e733e18dbefdb2b560543aa62162619f4f0c466b
Daniel Munoz <daniel.munozdev@gmail.com>
2019-03-05 22:39:57 -0800

    Updates

---
+++ precache-manifest.06b793b5fb40d3d0630a3471d31a175a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.71350252.js"
+  },
+  {
+    "revision": "42e94f9b676cc24321bc",
+    "url": "/react-todo/static/js/main.42e94f9b.chunk.js"
+  },
+  {
+    "revision": "5fea0a9e6b8b3a0fccd4",
+    "url": "/react-todo/static/js/2.5fea0a9e.chunk.js"
+  },
+  {
+    "revision": "42e94f9b676cc24321bc",
+    "url": "/react-todo/static/css/main.fb4dab3c.chunk.css"
+  },
+  {
+    "revision": "d99bd1351c681b7336c07f486a9ea5df",
+    "url": "/react-todo/index.html"
+  }
+];
